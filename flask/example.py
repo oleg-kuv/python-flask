043b69599ce64758fee7960a2891709795434d56
Oleg Kuvshinov <oleg@o-kuv.ru>
2023-08-31 15:09:22 +0300

    add 302 redirect

--- flask/example.py
+++ flask/example.py
@@ -1,17 +1,33 @@
-from flask import Flask
+from flask import Flask,request,make_response
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(request.headers)
     return 'Hello from Flask!'
 
 @app.get('/users')
 def users_get():
+    print(request.headers)
     return 'GET /users'
 
 @app.post('/users')
 def users_post():
+    print(request.headers)
     return 'POST /users'
+
+@app.route('/json/')
+def json():
+    return {'json': 42}
+
+@app.route('/members')
+def members():
+    response = make_response('members')
+    response.headers['location']='/users'
+    response.status_code = 302
+
+    return response
 
 app.run(host='0.0.0.0', port=8000)
